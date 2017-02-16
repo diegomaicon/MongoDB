2c735c9a6bb86d5ca3ef9135714b7212b351d373
Diego Maicon Silva <diego_maicon20@hotmail.com>
2017-02-16 10:13:16 -0200

    Testendo novo commit

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,9 +1,14 @@
+import com.sun.prism.shader.Mask_TextureSuper_AlphaTest_Loader;
+
 /**
  * Created by Diego on 16/02/2017.
  */
 public class Cliente {
 
     public static void main(String[] args) {
+
+        TEste
+
 
     }
 }
